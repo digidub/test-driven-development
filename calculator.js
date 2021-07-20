8d83c809fa4b3109aa1c6d2033aeba8d80cf7ee8
Sholto <madein.mania@gmail.com>
2021-07-20 12:15:13 +0100

    check for numbers

--- calculator.js
+++ calculator.js
@@ -1,13 +1,16 @@
 function calculator(x, y, operator) {
-  switch (operator) {
-    case 'add':
-      return x + y;
-    case 'subtract':
-      return x - y;
-    case 'multiply':
-      return x * y;
-    case 'divide':
-      return x / y;
+  if (typeof x === 'number' && typeof y === 'number') {
+    switch (operator) {
+      case 'add':
+        return x + y;
+      case 'subtract':
+        return x - y;
+      case 'multiply':
+        return x * y;
+      case 'divide':
+        return x / y;
+    }
   }
+  return 'ERROR: at least one operand is not in the right format';
 }
 module.exports = calculator;
