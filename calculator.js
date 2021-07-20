83299a03172ec0c94f90277e980cc0f4dc5f19a2
Sholto <madein.mania@gmail.com>
2021-07-20 12:09:22 +0100

    switch cases for all operators

--- calculator.js
+++ calculator.js
@@ -2,6 +2,12 @@ function calculator(x, y, operator) {
   switch (operator) {
     case 'add':
       return x + y;
+    case 'subtract':
+      return x - y;
+    case 'multiply':
+      return x * y;
+    case 'divide':
+      return x / y;
   }
 }
 module.exports = calculator;
