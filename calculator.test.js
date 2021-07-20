dc6f92525d51857d35f9ecfd2872cf716531cbe4
Sholto <madein.mania@gmail.com>
2021-07-20 12:17:33 +0100

    test for correct operators

--- calculator.test.js
+++ calculator.test.js
@@ -16,10 +16,14 @@ test('divides two numbers', () => {
   expect(calculator(100, 5, 'divide')).toBe(20);
 });
 
-test('only accepts with numbers', () => {
+test('only accepts numbers', () => {
   expect(calculator('hi', 5, 'divide')).toBe('ERROR: at least one operand is not in the right format');
 });
 
-test('only accepts with numbers', () => {
+test('only accepts numbers', () => {
   expect(calculator(3, 'chicken', 'divide')).toBe('ERROR: at least one operand is not in the right format');
+});
+
+test('checks for correct operators', () => {
+  expect(calculator(3, 4, 'egg')).toBe('ERROR: unknown operator provided');
 });
