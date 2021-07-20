2b2015d8745dce0430481db503df3723eae5cf70
Sholto <madein.mania@gmail.com>
2021-07-20 11:05:53 +0100

    test for multiple words

--- capitalize.test.js
+++ capitalize.test.js
@@ -7,3 +7,7 @@ test('capitalises word', () => {
 test('capitalises two words', () => {
   expect(capitalize('hello world')).toBe('Hello World');
 });
+
+test('capitalises several words', () => {
+  expect(capitalize('hello world how are you doing today')).toBe('Hello World How Are You Doing Today');
+});
