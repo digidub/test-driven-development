63bd9fe9db0101e3b3821417bce1d550cf869c00
Sholto <madein.mania@gmail.com>
2021-07-20 11:21:33 +0100

    added test features

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,11 @@ const reverseString = require('./reverseString');
 test('reverses simple string', () => {
   expect(reverseString('hello')).toBe('olleh');
 });
+
+test('reverses string with spaces', () => {
+  expect(reverseString('hello world')).toBe('dlrow olleh');
+});
+
+test('reverses string with spaces and punctuation', () => {
+  expect(reverseString('hello world!!')).toBe('!!dlrow olleh');
+});
