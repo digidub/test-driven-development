0144dbb79c2fd261481ecebcd545981e65d598c5
Sholto <madein.mania@gmail.com>
2021-07-20 11:04:50 +0100

    adjust function to capitalise multiple words in string

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,10 @@
 function capitalize(string) {
-  return string[0].toUpperCase() + string.substring(1);
+  const lowerCaseWords = string.split(' ');
+  const upperCaseWords = [];
+  lowerCaseWords.forEach((word) => {
+    upperCaseWords.push(word[0].toUpperCase() + word.substring(1));
+  });
+  const capitalizedString = upperCaseWords.join(' ');
+  return capitalizedString;
 }
 module.exports = capitalize;
