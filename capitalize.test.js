4f0cdb54ec27dcff4287c87969047507c6089294
Sholto <madein.mania@gmail.com>
2021-07-20 10:59:12 +0100

    test for capitalising two words

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require('./capitalize');
 test('capitalises word', () => {
   expect(capitalize('hello')).toBe('Hello');
 });
+
+test('capitalises two words', () => {
+  expect(capitalize('hello world')).toBe('Hello World');
+});
