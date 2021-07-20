fa8179971665637bf7a92f2b8f914f36bcf8840d
Sholto <madein.mania@gmail.com>
2021-07-20 12:11:05 +0100

    test for numbers

--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,11 @@ test('multiplies two numbers', () => {
 test('divides two numbers', () => {
   expect(calculator(100, 5, 'divide')).toBe(20);
 });
+
+test('only accepts with numbers', () => {
+  expect(calculator('hi', 5, 'divide')).toBe('ERROR: at least one operand is not in the right format');
+});
+
+test('only accepts with numbers', () => {
+  expect(calculator(3, 'chicken', 'divide')).toBe('ERROR: at least one operand is not in the right format');
+});
