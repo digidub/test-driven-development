3ca70d819be3127884e446d7bbb486d8de8d89c3
Sholto <madein.mania@gmail.com>
2021-07-20 12:09:16 +0100

    test for all operators

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,15 @@ const calculator = require('./calculator');
 test('adds two numbers', () => {
   expect(calculator(2, 4, 'add')).toBe(6);
 });
+
+test('subtracts two numbers', () => {
+  expect(calculator(8, 2, 'subtract')).toBe(6);
+});
+
+test('multiplies two numbers', () => {
+  expect(calculator(2, 3, 'multiply')).toBe(6);
+});
+
+test('divides two numbers', () => {
+  expect(calculator(100, 5, 'divide')).toBe(20);
+});
