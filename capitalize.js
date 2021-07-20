6b1becf8c3f0df8cbd1e16b100f6632862b99db7
Sholto <madein.mania@gmail.com>
2021-07-20 11:08:24 +0100

    function for empty string

--- capitalize.js
+++ capitalize.js
@@ -1,10 +1,12 @@
 function capitalize(string) {
-  const lowerCaseWords = string.split(' ');
-  const upperCaseWords = [];
-  lowerCaseWords.forEach((word) => {
-    upperCaseWords.push(word[0].toUpperCase() + word.substring(1));
-  });
-  const capitalizedString = upperCaseWords.join(' ');
-  return capitalizedString;
+  if (string) {
+    const lowerCaseWords = string.split(' ');
+    const upperCaseWords = [];
+    lowerCaseWords.forEach((word) => {
+      upperCaseWords.push(word[0].toUpperCase() + word.substring(1));
+    });
+    const capitalizedString = upperCaseWords.join(' ');
+    return capitalizedString;
+  } else return 'ERROR';
 }
 module.exports = capitalize;
